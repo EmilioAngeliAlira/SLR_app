46558446e91828c069d628415adc332733da4437
EmilioAngeliAlira <166006061+EmilioAngeliAlira@users.noreply.github.com>
2024-04-04 14:25:14 +0200

    Update Excerpts.py

--- Excerpts.py
+++ Excerpts.py
@@ -96,8 +96,7 @@ with col1.container(height=500):
         st.write(f"**Tables & figures:**")
         for image_dir in tables_and_figures:
             aux_name = os.path.basename(image_dir)
-            print(aux_name)
-            print(" ")
+            st.write(aux_name)
             image_dir = os.path.join(current_directory, "ExtractedTablesAndFigures", aux_name)
             st.image(image_dir + ".png")
     if not text_content and not tables_and_figures:
