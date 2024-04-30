b941f71e8289c3684eb3909a42bc966ff416171d
EmilioAngeliAlira <166006061+EmilioAngeliAlira@users.noreply.github.com>
2024-04-30 15:00:40 +0200

    Update PDF_view.py

--- pages/PDF_view.py
+++ pages/PDF_view.py
@@ -41,6 +41,21 @@ def show_pdf(pdf_path):
     except FileNotFoundError:
         st.error("PDF not found. Please check the file path.")
 
+def show_pdf(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    # Embedding PDF in HTML
+    pdf_display =  f"""<embed
+    class="pdfobject"
+    type="application/pdf"
+    title="Embedded PDF"
+    src="data:application/pdf;base64,{base64_pdf}"
+    style="overflow: auto; width: 100%; height: 100%;">"""
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
 
 # INDECES:
 current_directory  = os.getcwd()
